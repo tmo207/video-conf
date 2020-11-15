a3ac529260800f184b083896016c66a7fc03a103
timo.jancke <timo.jancke@xing.com>
2020-11-15 22:38:14 +0100

    fix(constants): host string should be 'referent'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,6 @@
-// export const APP_ID = 'faf886e398cb4bfdbd38868cfdf11f29';
-// export const CHANNEL_ID = 'react-test';
-// export const APP_ID = 'eb8e2114337f4422b490c61bdb758a98'; // dev
-// export const CHANNEL_ID = 'react-dev'; // dev
-// export const EVENT_ID = 'lr5PUQf9VbRUYXIoiZHJ';
-
-// export const USER_TOKEN =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ4aXJjdXMiLCJpYXQiOjE2MDUyODQ4MjQsImV4cCI6MTYwNTMwMDcxMjI0LCJ1aWQiOiIxOCIsIm5hbWUiOiJtYXhNdXN0ZXIiLCJyb2xlcyI6WyJCZXN1Y2hlciJdfQ.sV5DNJ_zXMSsunI8fI2ytAaQufgYGjt24NG1AIu1jVg';
-// export const HOST_TOKEN =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ4aXJjdXMiLCJpYXQiOjE2MDUyODI5NTQsImV4cCI6MTYwNTMwMDY5MzU0LCJ1aWQiOiIxMyIsIm5hbWUiOiJEYXZpZEtvZXJudGdlbiIsInJvbGVzIjpbImFkbWluaXN0cmF0b3IiXX0.ev5zO5hoaIYf7ptRWpPqE3bqDm3qJQ_UWOUXZZ8ieos';
-
 export const ROLES = {
-  SUPERHOST: 'superhost', // TODO: change to superuser
-  HOST: 'cohost', // TODO: change to referent
+  SUPERHOST: 'superhost',
+  HOST: 'referent',
   AUDIENCE: 'audience',
 };
 export const SCREEN_SHARE = 'screen-share';
